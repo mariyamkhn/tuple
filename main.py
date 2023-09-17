7a0614edda979f048811b8883ec4df1d8e9d9770
mariyamkhn <mariyamkhn85@gmail.com>
2023-09-17 12:30:44 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+#tuple data type
+students=('kumar','divya','rachel','sophia','sarah','david','christopher','kiran','mohan','sana','vidhi','meera','simran')
+print(students)
+#in tuple data type we use curly brackets
